4aa68991f8f05da824dd6c31f5d5fdb4abf707b3
KatiaGrasso <101561044+KatiaGrasso@users.noreply.github.com>
2023-12-30 17:43:32 +0100

    inizio schermata categorie

--- app/src/main/java/it/polito/database/screens/Category.kt
+++ app/src/main/java/it/polito/database/screens/Category.kt
@@ -1,33 +1,110 @@
 package it.polito.database.screens
 
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+
+
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-
+import androidx.compose.ui.unit.dp
 
 
 @Composable
     fun CategoryScreen(){
-        Box(
-            modifier = Modifier.fillMaxSize()
-                .background(Color.Black),
-            contentAlignment = Alignment.Center,
+    Greetings()
+    }
+@OptIn(ExperimentalMaterial3Api::class)
 
-        ){
-            Text(text = "Categorie",
-                color = MaterialTheme.colorScheme.primary,
-                fontSize = MaterialTheme.typography.headlineLarge.fontSize,
-                fontWeight = FontWeight.Bold
+@Composable
+// list of items
+private fun Greetings(
+    modifier: Modifier = Modifier,
+    names : List<String> = List(1000) { "$it" }
+) {
+    LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+        items(items = names) {name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+// ui for card
+@Composable
+private fun Greeting(name: String) {
+    Card(colors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary
+    ), modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)) {
+        CardContent(name = name)
+    }
+}
+
+// ui for card content
+@Composable
+private fun CardContent(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(text = "Hello, ")
+            Text(
+                text = name,
+                style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold)
             )
-
-
+            if (expanded) {
+                Text(text = ("Co,pose ipsum color sit laxy").repeat(4))
+            }
         }
 
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.KeyboardArrowDown else Icons.Filled.KeyboardArrowRight,
+                contentDescription =
+                if (expanded) {
+                    "show less"
+                } else {
+                    "show more"
+                }
+            )
+        }
     }
+}
