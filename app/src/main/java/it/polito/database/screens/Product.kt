6e66e546fcd6092e529f596f00caa01179d01a3a
Anna <annamariaf1999@gmail.com>
2024-01-21 18:10:06 +0100

    Pagina del prodotto: risistemato il layout

--- app/src/main/java/it/polito/database/screens/Product.kt
+++ app/src/main/java/it/polito/database/screens/Product.kt
@@ -1,13 +1,15 @@
 package it.polito.database.screens
 
 import android.annotation.SuppressLint
+import android.graphics.Paint
 import android.graphics.drawable.Icon
 import android.media.Image
+import android.text.Layout
+import android.widget.GridLayout
 import android.widget.ImageView
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -15,6 +17,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -50,6 +53,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -72,6 +76,7 @@ import it.polito.database.AppViewModel
 import it.polito.database.FindUrl
 import it.polito.database.R
 import it.polito.database.ui.theme.Screen
+import okhttp3.internal.wait
 
 @Composable
 fun ProductScreen(viewModel: AppViewModel) {
@@ -141,21 +146,9 @@ fun ProductContent(
                           bottom: ['bgCard', 'top'],
                           right: ['bgCard', 'right']
                         },
-                        productCategory: {
-                          top: ['parent', 'top'],
-                          left: ['parent', 'left']
-                        },
-                        productTitle: {
-                          top: ['productCategory', 'bottom'],
-                          left: ['parent', 'left']
-                        },
-                        productPrice: {
+                        btnHeart: {
                           bottom: ['bgCard', 'top'],
-                          left: ['parent', 'left']
-                        },
-                        productPriceLabel: {
-                        bottom: ['productCategory', 'top'],
-                        left: ['parent', 'left']
+                          right: ['parent', 'right'],
                         }
                     }
                 """.trimIndent()
@@ -167,50 +160,19 @@ fun ProductContent(
         Image(
         painter = painterResource(id = R.drawable.shaker700ml),
         contentDescription = "Product Image",
-        contentScale = ContentScale.FillBounds,
+        contentScale = ContentScale.FillWidth,
         modifier = Modifier
             .layoutId("productImage")
             .fillMaxSize()
-            .padding(top = 80.dp, bottom = 300.dp)
+            .padding(bottom = 300.dp)
     )
-        var filledHeart by remember{ mutableStateOf(false) }
-        FloatingActionButton(
-            onClick = {
-                if(filledHeart==false) {
-                    filledHeart = true
-                }else{
-                    filledHeart = false
-                }
-            },
-            containerColor = Color.Transparent,
-            modifier = Modifier
-                .layoutId("btnHeart")
-                .padding(top = 320.dp, start = 320.dp, bottom = 8.dp),
-            elevation = FloatingActionButtonDefaults.elevation(
-                defaultElevation = 0.dp,
-                pressedElevation = 0.dp
-            )
-        ) {
-            if(filledHeart==false) {
-                Image(
-                    imageVector = Icons.Outlined.FavoriteBorder,
-                    contentDescription = "Empty Heart",
-                    modifier = Modifier.size(50.dp)
-                )}else{
-                Image(
-                    imageVector = Icons.Outlined.Favorite,
-                    contentDescription = "Filled Heart",
-                    modifier = Modifier.size(50.dp)
-                )
-            }
-
-        }
 
         Card(
             shape = RoundedCornerShape(
                 topStart = 24.dp,
                 topEnd = 24.dp
             ),
+            colors = CardDefaults.cardColors(Color.DarkGray),
             elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
             modifier = Modifier
                 .layoutId("bgCard")
@@ -222,8 +184,20 @@ fun ProductContent(
                     content =
                     """
                     {
-                        txtColor: {
+                       productCategory: {
                           top: ['parent', 'top'],
+                          left: ['parent', 'left']
+                        },
+                        productTitle: {
+                          top: ['productCategory', 'bottom'],
+                          left: ['parent', 'left']
+                        },
+                        productPrice: {
+                          top: ['parent', 'top'],
+                          right: ['parent', 'right'],
+                        },
+                        txtColor: {
+                          top: ['productTitle', 'top'],
                           left: ['parent', 'left']
                         },
                         txtSize: {
@@ -234,8 +208,12 @@ fun ProductContent(
                           top: ['txtSize', 'bottom'],
                           left: ['txtSize', 'left']
                         },
+                        txtDescriptionTitle: {
+                          top: ['txtSizeValue', 'bottom'],
+                          left: ['parent', 'left']
+                        },
                         txtDescription: {
-                          top: ['txtSizeValue', 'top'],
+                          top: ['txtDescriptionTitle', 'bottom'],
                           left: ['parent', 'left'],
                           right: ['parent', 'right']
                         },
@@ -252,18 +230,8 @@ fun ProductContent(
                           top: ['txtDescription', 'bottom'],
                           left: ['txtQtd', 'right']
                         },
-                        btnHeart: {
-                          top: ['btnPlus', 'top'],
-                          bottom: ['btnPlus', 'bottom'],
-                          right: ['parent', 'right']
-                        },
-                        btnAddToCard: {
-                          top: ['btnMinus', 'bottom'],
-                          left: ['parent', 'left']
-                        },
                         btnBuy: {
                           top: ['btnMinus', 'bottom'],
-                          left: ['btnAddToCart', 'right'],
                           right: ['parent', 'right']
                         },
                         color1: {
@@ -290,53 +258,78 @@ fun ProductContent(
               CardContent()
             }
         }
-        Text(
-            text = "Accessori per l'allenamento",
+        var filledHeart by remember{ mutableStateOf(false) }
+        FloatingActionButton(
+            onClick = {
+                if(filledHeart==false) {
+                    filledHeart = true
+                }else{
+                    filledHeart = false
+                }
+            },
+            containerColor = Color.Transparent,
             modifier = Modifier
-                .layoutId("productCategory")
-                .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 16.dp),
-            fontSize = TextUnit(20F, TextUnitType.Sp),
-            color = Color.White
+                .layoutId("btnHeart")
+                .padding(top = 32.dp, start = 320.dp, bottom = 8.dp, end = 16.dp),
+            elevation = FloatingActionButtonDefaults.elevation(
+                defaultElevation = 0.dp,
+                pressedElevation = 0.dp
             )
+        ) {
+            if(filledHeart==false) {
+                Image(
+                    imageVector = Icons.Outlined.FavoriteBorder,
+                    contentDescription = "Empty Heart",
+                    modifier = Modifier.size(50.dp)
+                )}else{
+                Image(
+                    imageVector = Icons.Outlined.Favorite,
+                    contentDescription = "Filled Heart",
+                    modifier = Modifier.size(50.dp)
+                )
+            }
 
-        Text(
-            text = "Shaker per proteine 700ml",
-            fontSize = TextUnit(30F, TextUnitType.Sp),
-            color = Color.White,
-            modifier = Modifier
-                .layoutId("productName")
-                .padding(start = 16.dp, top = 40.dp)
-        )
+        }
 
-        Text(
-            text = "Price",
-            fontSize = 20.sp,
-            color = Color.White,
-            modifier = Modifier
-                .layoutId("productPriceLabel")
-                .padding(start = 16.dp)
-        )
-
-        Text(
-            text = "36€",
-            fontSize = 40.sp,
-            fontWeight = FontWeight.Bold,
-            color = Color.Black,
-            modifier = Modifier
-                .layoutId("productPrice")
-                .padding(start = 16.dp, bottom = 8.dp, top = 8.dp)
-        )
     }
 }
 
 @Composable
 fun CardContent(){
     Text(
-        text = "Color",
+        text = "Accessori per l'allenamento",
+        modifier = Modifier
+            .layoutId("productCategory")
+            .padding(top = 20.dp, bottom = 16.dp),
+        fontSize = TextUnit(20F, TextUnitType.Sp),
+        color = Color.White
+    )
+
+    Text(
+        text = "Shaker per proteine",
+        fontSize = TextUnit(30F, TextUnitType.Sp),
+        color = Color.White,
+        modifier = Modifier
+            .layoutId("productName")
+            .padding(top = 40.dp)
+    )
+
+    Text(
+        text = "36€",
+        fontSize = 40.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color.White,
+        modifier = Modifier
+            .layoutId("productPrice")
+            .padding(end = 16.dp, top = 32.dp)
+    )
+
+    Text(
+        text = "Colore",
         color = Color.Gray,
         modifier = Modifier
             .layoutId("txtColor")
-            .padding(top = 64.dp)
+            .padding(top = 40.dp)
     )
 
     Button(
@@ -345,8 +338,8 @@ fun CardContent(){
         modifier = Modifier
             .layoutId("color1")
             .padding(top = 8.dp)
-            .width(30.dp)
-            .height(30.dp),
+            .width(20.dp)
+            .height(20.dp),
         colors = ButtonDefaults.buttonColors(Color.Black),
         elevation = ButtonDefaults.buttonElevation(
             defaultElevation = 0.dp,
@@ -362,8 +355,8 @@ fun CardContent(){
         modifier = Modifier
             .layoutId("color2")
             .padding(top = 8.dp, start = 20.dp)
-            .width(30.dp)
-            .height(30.dp),
+            .width(20.dp)
+            .height(20.dp),
         colors = ButtonDefaults.buttonColors(Color.White),
         elevation = ButtonDefaults.buttonElevation(
             defaultElevation = 0.dp,
@@ -373,42 +366,75 @@ fun CardContent(){
     ) {
 
     }
-    OutlinedButton(
-        onClick = { /*TODO*/ },
-        shape = RoundedCornerShape(16.dp),
-        modifier = Modifier
-            .layoutId("btnMinus")
-            .padding(top = 32.dp, end = 16.dp)
-    ) {
-        Text(
-            text = "-",
-            fontSize = 23.sp,
-            color = Color.Black
-        )
-    }
 
     Text(
-        text = "01",
-        fontSize = 23.sp,
-        color = Color.DarkGray,
+        text = "Dimensione",
         modifier = Modifier
-            .layoutId("txtQtd")
-            .padding(top = 32.dp, start = 80.dp)
+            .layoutId("txtSize")
+            .padding(top = 40.dp, start = 90.dp),
+        color = Color.Gray
     )
-    IconButton(
-        onClick = { /*TODO*/ },
-        colors = IconButtonDefaults.iconButtonColors(Color.Black),
-        modifier = Modifier
-            .layoutId("btnPlus")
-            .padding(top = 32.dp, start = 120.dp)
-    ) {
-       androidx.compose.material3.Icon(
-           imageVector = Icons.Default.Add,
-           contentDescription = "Aggiungi quantità",
-           tint = Color.White
-       )
-    }
 
+    Text(
+        text = "700ml",
+        modifier = Modifier
+            .layoutId("txtSizeValue")
+            .padding(start = 90.dp, top = 8.dp, bottom = 16.dp),
+        color = Color.Gray
+    )
+
+    Text(
+        text = "Descrizione",
+        fontWeight = FontWeight.SemiBold,
+        modifier = Modifier
+            .layoutId("txtDescriptionTitle"),
+        color = Color.Gray
+    )
+
+    Text(
+        text = "Lo shaker proteico permette di mescolare rapidamente e facilmente le proteine in polvere; versare il latte o l'acqua, aggiungere la polvere, chiudere, agitare e la bevanda proteica è subito pronta.",
+        modifier = Modifier
+            .layoutId("txtDescription")
+            .padding(top = 8.dp),
+        color = Color.Gray
+    )
+
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .layoutId("btnMinus")
+                .padding(top = 16.dp, end = 16.dp)
+        ) {
+            Text(
+                text = "-",
+                fontSize = 23.sp,
+                color = Color.White
+            )
+        }
+
+        Text(
+            text = "1",
+            fontSize = 23.sp,
+            color = Color.LightGray,
+            modifier = Modifier
+                .layoutId("txtQtd")
+                .padding(top = 16.dp, start = 8.dp)
+        )
+        OutlinedButton(
+            onClick = { /*TODO*/ },
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier
+                .layoutId("btnPlus")
+                .padding(top = 16.dp, start = 16.dp)
+        ) {
+            Text(
+                text = "+",
+                fontSize = 23.sp,
+                color = Color.White
+            )
+        }
+/*
     OutlinedButton(
         onClick = { /*TODO*/ },
         border = BorderStroke(
@@ -417,13 +443,34 @@ fun CardContent(){
         ),
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier
-            .layoutId("btnAddtoCart")
+            .layoutId("btnAddToCart")
             .padding(top = 32.dp)
     ) {
         Image(
             imageVector = Icons.Default.ShoppingCart,
             contentDescription = "Add Cart",
             modifier = Modifier.size(25.dp)
+        )
+    }
+*/
+    Button(
+        onClick = {/*TODO*/},
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier
+            .layoutId("btnBuy")
+            .padding(top = 16.dp, start = 90.dp)
+            .width(275.dp),
+        colors = ButtonDefaults.buttonColors(Color.Yellow),
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+            disabledElevation = 0.dp
+        )
+    ){
+        Text(
+            text = "Aggiungi al carrello",
+            fontSize = 23.sp,
+            color = Color.Black
         )
     }
 }
