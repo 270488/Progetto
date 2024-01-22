7919ca50cb5846daac038ec4a59f9b63a3381cec
Anna <annamariaf1999@gmail.com>
2024-01-22 13:23:16 +0100

    Pagina Prodotto: menu a tendina e scroll

--- app/src/main/java/it/polito/database/screens/Product.kt
+++ app/src/main/java/it/polito/database/screens/Product.kt
@@ -1,53 +1,36 @@
 package it.polito.database.screens
 
 import android.annotation.SuppressLint
-import android.graphics.Paint
-import android.graphics.drawable.Icon
-import android.media.Image
-import android.text.Layout
-import android.widget.GridLayout
-import android.widget.ImageView
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
-import androidx.compose.material.icons.rounded.ArrowBack
-import androidx.compose.material.icons.rounded.KeyboardArrowLeft
-import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.IconButtonColors
-import androidx.compose.material3.IconButtonDefaults
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -56,27 +39,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.widget.ConstraintLayout
-import coil.decode.ImageSource
 import it.polito.database.AppViewModel
-import it.polito.database.FindUrl
 import it.polito.database.R
-import it.polito.database.ui.theme.Screen
-import okhttp3.internal.wait
 
 @Composable
 fun ProductScreen(viewModel: AppViewModel) {
@@ -169,15 +142,16 @@ fun ProductContent(
 
         Card(
             shape = RoundedCornerShape(
-                topStart = 24.dp,
-                topEnd = 24.dp
+                topStart = 8.dp,
+                topEnd = 8.dp
             ),
             colors = CardDefaults.cardColors(Color.DarkGray),
             elevation = CardDefaults.cardElevation(defaultElevation = 0.dp),
             modifier = Modifier
                 .layoutId("bgCard")
-                .height(380.dp)
+                .height(400.dp)
                 .fillMaxWidth()
+                .verticalScroll(rememberScrollState())
         ) {
             ConstraintLayout(
                 constraintSet = ConstraintSet(
@@ -196,56 +170,31 @@ fun ProductContent(
                           top: ['parent', 'top'],
                           right: ['parent', 'right'],
                         },
-                        txtColor: {
+                        ratings: {
                           top: ['productTitle', 'top'],
                           left: ['parent', 'left']
                         },
-                        txtSize: {
-                          top: ['txtColor', 'top'],
-                          left: ['txtColor', 'right']
-                        },
-                        txtSizeValue: {
-                          top: ['txtSize', 'bottom'],
-                          left: ['txtSize', 'left']
-                        },
-                        txtDescriptionTitle: {
-                          top: ['txtSizeValue', 'bottom'],
+                        menuOptions: {
+                          top: ['ratings', 'top'],
                           left: ['parent', 'left']
+                        },
+                        menuQty: {
+                          top: ['menuOptions', 'top'],
+                          left: ['menuOptions', 'right']
+                        }, 
+                        txtDescriptionTitle: {
+                          top: ['menuOptions', 'bottom'],
+                          left: ['parent', 'left'],
                         },
                         txtDescription: {
                           top: ['txtDescriptionTitle', 'bottom'],
                           left: ['parent', 'left'],
                           right: ['parent', 'right']
                         },
-                        btnMinus: {
-                          top: ['txtDescription', 'bottom'],
-                          left: ['parent', 'left']
-                        },
-                        txtQtd: {
-                          top: ['btnMinus', 'top'],
-                          bottom: ['btnMinus','bottom'],
-                          left: ['btnMinus', 'right']
-                        },
-                        btnPlus: {
-                          top: ['txtDescription', 'bottom'],
-                          left: ['txtQtd', 'right']
-                        },
                         btnBuy: {
-                          top: ['btnMinus', 'bottom'],
+                          top: ['txtDescription', 'bottom'],
                           right: ['parent', 'right']
-                        },
-                        color1: {
-                          top: ['txtColor', 'bottom'],
-                          left: ['parent', 'left']
-                        },
-                        color2: {
-                          top: ['txtColor', 'bottom'],
-                          left: ['color1', 'right']
-                        },
-                        color3: {
-                          top: ['txtColor', 'bottom'],
-                          left: ['color2', 'right']
-                        },
+                        }
                     }
                 """.trimIndent()
                 ),
@@ -300,166 +249,81 @@ fun CardContent(){
         text = "Accessori per l'allenamento",
         modifier = Modifier
             .layoutId("productCategory")
-            .padding(top = 20.dp, bottom = 16.dp),
-        fontSize = TextUnit(20F, TextUnitType.Sp),
+            .padding(top = 10.dp, bottom = 16.dp),
+        fontSize = 14.sp,
         color = Color.White
     )
 
     Text(
-        text = "Shaker per proteine",
-        fontSize = TextUnit(30F, TextUnitType.Sp),
+        text = "Shaker",
+        fontSize = 32.sp,
+        fontWeight = FontWeight.Bold,
         color = Color.White,
         modifier = Modifier
             .layoutId("productName")
-            .padding(top = 40.dp)
+            .padding(top = 30.dp)
     )
 
     Text(
         text = "36€",
-        fontSize = 40.sp,
+        fontSize = 36.sp,
         fontWeight = FontWeight.Bold,
         color = Color.White,
         modifier = Modifier
             .layoutId("productPrice")
-            .padding(end = 16.dp, top = 32.dp)
+            .padding(end = 16.dp, top = 25.dp)
     )
 
-    Text(
-        text = "Colore",
-        color = Color.Gray,
+    Row(
         modifier = Modifier
-            .layoutId("txtColor")
-            .padding(top = 40.dp)
-    )
-
-    Button(
-        onClick = { /*TODO*/ },
-        shape = RoundedCornerShape(20.dp),
-        modifier = Modifier
-            .layoutId("color1")
-            .padding(top = 8.dp)
-            .width(20.dp)
-            .height(20.dp),
-        colors = ButtonDefaults.buttonColors(Color.Black),
-        elevation = ButtonDefaults.buttonElevation(
-            defaultElevation = 0.dp,
-            pressedElevation = 0.dp,
-            disabledElevation = 0.dp
-        )
-    ) {
+            .fillMaxWidth()
+            .padding(top = 120.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+       // RatingBar()
     }
-
-    Button(
-        onClick = { /*TODO*/ },
-        shape = RoundedCornerShape(20.dp),
+    Row(
         modifier = Modifier
-            .layoutId("color2")
-            .padding(top = 8.dp, start = 20.dp)
-            .width(20.dp)
-            .height(20.dp),
-        colors = ButtonDefaults.buttonColors(Color.White),
-        elevation = ButtonDefaults.buttonElevation(
-            defaultElevation = 0.dp,
-            pressedElevation = 0.dp,
-            disabledElevation = 0.dp
-        )
-    ) {
-
+            .fillMaxWidth()
+            .padding(top = 140.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        OptionSelection()
+        QtySelection()
     }
-
-    Text(
-        text = "Dimensione",
-        modifier = Modifier
-            .layoutId("txtSize")
-            .padding(top = 40.dp, start = 90.dp),
-        color = Color.Gray
-    )
-
-    Text(
-        text = "700ml",
-        modifier = Modifier
-            .layoutId("txtSizeValue")
-            .padding(start = 90.dp, top = 8.dp, bottom = 16.dp),
-        color = Color.Gray
-    )
 
     Text(
         text = "Descrizione",
         fontWeight = FontWeight.SemiBold,
         modifier = Modifier
-            .layoutId("txtDescriptionTitle"),
-        color = Color.Gray
+            .layoutId("txtDescriptionTitle")
+            .padding(top = 160.dp),
+        color = Color.White
     )
 
     Text(
-        text = "Lo shaker proteico permette di mescolare rapidamente e facilmente le proteine in polvere; versare il latte o l'acqua, aggiungere la polvere, chiudere, agitare e la bevanda proteica è subito pronta.",
+        text = "Lo shaker proteico permette di mescolare rapidamente e facilmente le proteine in polvere; versare il latte o l'acqua, aggiungere la polvere, chiudere, agitare e la bevanda proteica è subito pronta.\n" +
+                "\n" +
+                "L'agitatore con setaccio previene la formazione di grumi e residui; lo shaker da palestra presenta inoltre una pratica scala a livelli di 50 ml fino a 700 ml\n" +
+                "\n" +
+                "Lo shaker proteico IronMaxx con filtro e tappo a vite è particolarmente adatto per i viaggi ed è pratico da portare nella borsa sportiva\n" +
+                "\n" +
+                "È possibile riempire e pulire con facilità lo shaker per frullati di proteine rimuovendo il grande tappo a vite: l'apertura per bere si trova sotto il tappo a vite più piccolo\n" +
+                "\n" +
+                "Cos'è incluso: 1 x IronMaxx Protein Shaker da 700 ml - Shaker Stabile con Setaccio per Frullati Proteici - Facile da Pulire - Color Nero Notte",
         modifier = Modifier
             .layoutId("txtDescription")
             .padding(top = 8.dp),
-        color = Color.Gray
+        color = Color.White
     )
 
-        OutlinedButton(
-            onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(16.dp),
-            modifier = Modifier
-                .layoutId("btnMinus")
-                .padding(top = 16.dp, end = 16.dp)
-        ) {
-            Text(
-                text = "-",
-                fontSize = 23.sp,
-                color = Color.White
-            )
-        }
-
-        Text(
-            text = "1",
-            fontSize = 23.sp,
-            color = Color.LightGray,
-            modifier = Modifier
-                .layoutId("txtQtd")
-                .padding(top = 16.dp, start = 8.dp)
-        )
-        OutlinedButton(
-            onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(16.dp),
-            modifier = Modifier
-                .layoutId("btnPlus")
-                .padding(top = 16.dp, start = 16.dp)
-        ) {
-            Text(
-                text = "+",
-                fontSize = 23.sp,
-                color = Color.White
-            )
-        }
-/*
-    OutlinedButton(
-        onClick = { /*TODO*/ },
-        border = BorderStroke(
-            width = 1.dp,
-            color = Color.White
-        ),
-        shape = RoundedCornerShape(16.dp),
-        modifier = Modifier
-            .layoutId("btnAddToCart")
-            .padding(top = 32.dp)
-    ) {
-        Image(
-            imageVector = Icons.Default.ShoppingCart,
-            contentDescription = "Add Cart",
-            modifier = Modifier.size(25.dp)
-        )
-    }
-*/
     Button(
         onClick = {/*TODO*/},
-        shape = RoundedCornerShape(16.dp),
+        shape = RoundedCornerShape(8.dp),
         modifier = Modifier
             .layoutId("btnBuy")
-            .padding(top = 16.dp, start = 90.dp)
-            .width(275.dp),
+            .padding(top = 32.dp, start = 90.dp, bottom = 16.dp)
+            .width(250.dp),
         colors = ButtonDefaults.buttonColors(Color.Yellow),
         elevation = ButtonDefaults.buttonElevation(
             defaultElevation = 0.dp,
@@ -475,3 +339,141 @@ fun CardContent(){
     }
 }
 
+@Composable
+fun RatingBar(
+    modifier: Modifier = Modifier,
+    rating: Double = 0.0,
+    stars: Int = 5,
+    starsColor: Color = Color.Yellow,
+    onRatingChange: (Double) -> Unit
+    ) {
+
+    var isHalfStar = (rating % 1) != 0.0
+    Row {
+        for(index in 1 .. stars){
+          /*  Icon(
+                imageVector = if(index<= rating){
+                        Icons.Rounded.Star
+                    } else {
+                           if(isHalfStar){
+                               Icons.Rounded
+                           } else {
+
+                           }
+                          },
+                modifier = Modifier.clickable { onRatingChange(index.toDouble()) },
+                contentDescription = null,
+                tint = starsColor,
+
+            ) */
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OptionSelection() {
+    var selectedOption by remember {
+        mutableStateOf<String>("Opzioni")
+    }
+    var isExpanded by remember {
+        mutableStateOf(false) //default: menù chiuso
+    }
+    val options = listOf("Opzione 1","Opzione 2","Opzione 3", "Opzione 4")
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .layoutId("menuOptions")
+            .width(160.dp)
+            .height(48.dp)
+    ){
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = it }
+        ) {
+            TextField(
+                value = selectedOption,
+                onValueChange = {},
+                readOnly = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(containerColor = Color.Gray, textColor = Color.White),
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                }, modifier = Modifier.menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false },
+                modifier = Modifier
+                    .height(100.dp)
+                    .background(Color.Gray)
+            ) {
+                options.forEach { o ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = o, color = Color.White)
+                        },
+                        onClick = {
+                            selectedOption = o
+                            isExpanded =
+                                false
+                        })
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun QtySelection() {
+    var selectedQty by remember {
+        mutableStateOf<String>("Quantità")
+    }
+    var isExpanded by remember {
+        mutableStateOf(false) //default: menù chiuso
+    }
+    val qty = listOf(1,2,3,4)
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .layoutId("menuQty")
+            .width(160.dp)
+            .height(48.dp)
+    ){
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange = { isExpanded = it }
+        ) {
+            TextField(
+                value = selectedQty,
+                onValueChange = {},
+                readOnly = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(containerColor = Color.Gray, textColor = Color.White),
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                }, modifier = Modifier.menuAnchor()
+            )
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false },
+                modifier = Modifier
+                    .height(100.dp)
+                    .background(Color.Gray)
+            ) {
+                qty.forEach { q ->
+                    DropdownMenuItem(
+                        text = {
+                            Text(text = q.toString(), color = Color.White)
+                        },
+                        onClick = {
+                            selectedQty = q.toString()
+                            isExpanded =
+                                false
+                        })
+                }
+            }
+        }
+    }
+}
