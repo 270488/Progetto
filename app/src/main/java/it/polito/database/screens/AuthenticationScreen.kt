37ae2b78ee4aaf50c849481e58b5139055c48c76
KatiaGrasso <101561044+KatiaGrasso@users.noreply.github.com>
2024-01-24 22:43:38 +0100

    password a puntini per log in

--- app/src/main/java/it/polito/database/screens/AuthenticationScreen.kt
+++ app/src/main/java/it/polito/database/screens/AuthenticationScreen.kt
@@ -1,13 +1,9 @@
 package it.polito.database.screens
 
 import android.util.Log
-import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.End
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -21,9 +17,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
@@ -31,17 +25,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import it.polito.database.ui.theme.Screen
@@ -114,6 +105,7 @@ fun AuthenticationScreen(navController: NavHostController,context: Authenticatio
             OutlinedTextField(
                 value = passwordValue.value,
                 shape = MaterialTheme.shapes.large,
+                visualTransformation = PasswordVisualTransformation(),
                 placeholder =
                 { Text(
                     text = "Password",
