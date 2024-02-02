0c1a8c01d4142829a775b3191130ab528319a1ff
andredelu98 <andrea.deluca1108@gmail.com>
2024-02-02 23:15:28 +0100

    Styling ProfilePage

--- app/src/main/java/it/polito/database/screens/Profile.kt
+++ app/src/main/java/it/polito/database/screens/Profile.kt
@@ -2,78 +2,111 @@ package it.polito.database.screens
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import it.polito.database.AppViewModel
 import it.polito.database.R
 import it.polito.database.ui.theme.Screen
+import it.polito.database.ui.theme.fontFamily
 
 
 @Composable
 fun ProfileScreen(viewModel: AppViewModel, navController: NavHostController) {
     Column(
         modifier = Modifier
-            .padding(top = 86.dp)
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(vertical = 100.dp)
     ) {
         // Icona e Scritte
         Row(
+            verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(horizontal = 16.dp)
         ) {
             // Icona
-            Column(
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "Profile icon",
                 modifier = Modifier
-                    .weight(.5F)
-            ) {
-                val profileIcon = R.drawable.profile
-                Image(
-                    painter = painterResource(id = profileIcon),
-                    contentDescription = "Profile icon",
-                    modifier = Modifier
-                        .size(150.dp)
-                        .clip(MaterialTheme.shapes.medium)
-                )
-            }
-
+                    .weight(0.35f)
+                    .border(1.dp, MaterialTheme.colorScheme.tertiary, CircleShape)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
             // Scritte
-            Column(
+            Box(
                 modifier = Modifier
-                    .weight(.5F)
-                    .padding(top = 36.dp)
-            ) {
-                Text(text = "Ciao,", modifier = Modifier.fillMaxWidth())
-                Text(text = "Pippo", modifier = Modifier.fillMaxWidth())
+                    .height(90.dp)
+                    .weight(0.65f)
+            ){
+                Text(
+                    text = buildAnnotatedString {
+                        append("Ciao,\n")
+                        withStyle(
+                            style = SpanStyle(
+                                color = MaterialTheme.colorScheme.tertiary,
+                                fontSize = 34.sp
+                            )
+                        ){
+                            append("Pippo")
+                        }
+                    },
+                    fontFamily = fontFamily,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    lineHeight = 40.sp,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.TopStart)
+                )
                 Text(
                     text = "Cambia account/Esci",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    fontSize = 14.sp,
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Normal,
                     modifier = Modifier
                         .clickable {
                             navController.navigate(Screen.AuthenticationScreen.route)
-
                             //TODO aggiungere un alert per chiedere conferma
                         }
-                        .fillMaxWidth()
+                        .align(Alignment.BottomEnd)
                 )
             }
         }
-
+        Spacer(modifier = Modifier.height(60.dp))
         // Card con opzioni
         Opzioni(navController)
     }
@@ -88,38 +121,45 @@ fun Opzioni(navController: NavHostController) {
         "Il mio account",
         "Gestisci FitLocker"
     )
-    elenco.forEach { name ->
-        Row(
-            modifier = Modifier
-                .padding(12.dp)
-                .animateContentSize()
-        )  {
-
-            Column(
+    Column {
+        Divider(thickness = 1.dp, color = MaterialTheme.colorScheme.tertiary)
+        elenco.forEach { name ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
-                    .weight(1f)
+                    .background(MaterialTheme.colorScheme.secondary)
                     .padding(12.dp)
-            ) {
+                    .fillMaxWidth()
+                    .animateContentSize()
+            )  {
                 Text(
                     text = name,
-                    style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Medium)
+                    fontFamily = fontFamily,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary
                 )
-            }
 
-            IconButton(onClick = {
-                if (name =="I miei preferiti" )
-                    navController.navigate(Screen.FavoritesScreen.route)
-                else if (name =="I miei resi" )
-                    navController.navigate(Screen.ResiScreen.route)
-                else if (name == "Il mio account")
-                    navController.navigate(Screen.AccountScreen.route)
-                //TODO else if () per le altre sezioni
-            }) {
-                Icon(
-                    imageVector =  Icons.Filled.KeyboardArrowRight,
-                    contentDescription = "selezione"
-                )
+                IconButton(onClick = {
+                    if (name =="I miei preferiti" )
+                        navController.navigate(Screen.FavoritesScreen.route)
+                    else if (name =="I miei resi" )
+                        navController.navigate(Screen.ResiScreen.route)
+                    else if (name == "Il mio account")
+                        navController.navigate(Screen.AccountScreen.route)
+                    //TODO else if () per le altre sezioni
+                }) {
+                    Icon(
+                        painter =  painterResource(id = R.drawable.frecciadx),
+                        tint = MaterialTheme.colorScheme.tertiary,
+                        contentDescription = "selezione",
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
             }
+            Divider(thickness = 1.dp, color = MaterialTheme.colorScheme.tertiary)
         }
     }
+
 }
