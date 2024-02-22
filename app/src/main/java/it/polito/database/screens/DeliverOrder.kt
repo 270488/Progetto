f019b420f04f99c620f68f2152e31f47eeb9e9a8
270488 <s270488@studenti.polito.it>
2024-02-22 20:38:46 +0100

    fiz gestione arduino

--- app/src/main/java/it/polito/database/screens/DeliverOrder.kt
+++ app/src/main/java/it/polito/database/screens/DeliverOrder.kt
@@ -34,74 +34,17 @@ import it.polito.database.ui.theme.fontFamily
 
 @Composable
 fun DeliverOrder(viewModel: AppViewModel, navController: NavController) {
-    var sblocco by remember { mutableStateOf(false) }
-    var sbloccoPP by remember { mutableStateOf(0L) }
-    var sbloccoPG by remember { mutableStateOf(0L) }
-    var codiceDB =
-        database.child("ordini").child(viewModel.ordineSelezionato).child("CodiceSbloccoFattorino")
-    var codice by remember {
-        mutableStateOf("")
-    }
-    codiceDB.addValueEventListener(object : ValueEventListener {
+    var sportello by remember { mutableStateOf("") }
+    var ordine=database.child("ordini").child(viewModel.ordineSelezionato).child("Sportello")
+    ordine.addValueEventListener(object: ValueEventListener {
+        var sp=""
         override fun onDataChange(dataSnapshot: DataSnapshot) {
-            codice = dataSnapshot.value.toString()
+            sp=dataSnapshot.value.toString()
         }
-
         override fun onCancelled(databaseError: DatabaseError) {
             println("Errore nel leggere i dati dal database: ${databaseError.message}")
         }
-    })
 
-    viewModel.variabili.variabiliPath.addValueEventListener(object : ValueEventListener {
-        override fun onDataChange(dataSnapshot: DataSnapshot) { //fa una foto al db in quel momento e la mette in dataSnapshot
-            viewModel.variabili.CodeP = dataSnapshot.child("CodeP").value.toString()
-            viewModel.variabili.CodeG = dataSnapshot.child("CodeG").value.toString()
-            viewModel.variabili.CodiceTastierino =
-                dataSnapshot.child("CodiceTastierino").value.toString()
-            viewModel.variabili.PPAperta = dataSnapshot.child("PPAperta").value as Long
-            viewModel.variabili.PGAperta = dataSnapshot.child("PGAperta").value as Long
-            viewModel.variabili.Sblocco = dataSnapshot.child("Sblocco").value as Long
-            viewModel.variabili.SportelloP = dataSnapshot.child("SportelloP").value as Boolean
-            viewModel.variabili.SportelloG = dataSnapshot.child("SportelloG").value as Boolean
-            viewModel.variabili.CodiceErrato = dataSnapshot.child("CodiceErrato").value as Long
-
-            println("CodeP: " + viewModel.variabili.CodeP)
-            println("CodeG: " + viewModel.variabili.CodeG)
-            println("CodiceTastierino: " + viewModel.variabili.CodiceTastierino)
-            println("PPAPerta: " + viewModel.variabili.PPAperta)
-            println("PGAperta: " + viewModel.variabili.PGAperta)
-            println("Sblocco: " + viewModel.variabili.Sblocco)
-            println("SportelloP: " + viewModel.variabili.SportelloP)
-            println("SportelloG: " + viewModel.variabili.SportelloG)
-
-            if (viewModel.variabili.PGAperta == 0L && sbloccoPG == 1L) { //La porta si chiude
-                sbloccoPG = 0L
-                database.child("ordini").child(viewModel.ordineSelezionato).child("stato")
-                    .setValue("consegnato")
-                database.child("utenti").child(viewModel.uid).child("ordini")
-                    .child(viewModel.ordineSelezionato).setValue("consegnato")
-
-            } else if (viewModel.variabili.PGAperta == 1L && sbloccoPG == 0L) {
-                sbloccoPG = 1L
-            }
-            if (viewModel.variabili.PPAperta == 0L && sbloccoPP == 1L) { //La porta si chiude
-                sbloccoPP = 0L
-                database.child("ordini").child(viewModel.ordineSelezionato).child("stato")
-                    .setValue("consegnato")
-                database.child("utenti").child(viewModel.uid).child("ordini")
-                    .child(viewModel.ordineSelezionato).setValue("consegnato")
-            }
-            if (viewModel.variabili.PPAperta == 1L && sbloccoPP == 0L) {
-                sbloccoPP = 1L
-            }
-
-
-        }
-
-        override fun onCancelled(databaseError: DatabaseError) {
-            // Gestisci gli errori qui
-            println("Errore nel leggere i dati dal database: ${databaseError.message}")
-        }
     })
 
     Column(
@@ -118,32 +61,11 @@ fun DeliverOrder(viewModel: AppViewModel, navController: NavController) {
             modifier = Modifier
                 .padding(horizontal = 20.dp)
         ) {
-            if (sblocco) {
-                Row(
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.Top,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .background(Color.Gray)
-                ) {
 
-                    Text(
-                        text = "Il codice per il ritiro è:\n" + codice,
-                        fontFamily = fontFamily,
-                        color = Color.White,
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Start
-                    )
-                }
+            Button(onClick = { //TODO cambiare valori variabili di sblocco nel DB
 
 
-            }
-
-            Button(onClick = {
-                sblocco = true;
-                viewModel.variabili.Sblocco = 1L;
-                writeVariables(viewModel.variabili)
+                //writeVariables(viewModel.variabili)
             }) {
                 Text(text = "Sblocca Locker")
             }
