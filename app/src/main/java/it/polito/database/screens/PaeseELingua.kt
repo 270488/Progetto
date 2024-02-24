45e1fe6e54b1dcc1e14894623846583574238ab6
andredelu98 <andrea.deluca1108@gmail.com>
2024-02-24 01:32:01 +0100

    Styling PaeseELingua

--- app/src/main/java/it/polito/database/screens/PaeseELingua.kt
+++ app/src/main/java/it/polito/database/screens/PaeseELingua.kt
@@ -12,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
@@ -30,7 +32,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -38,6 +43,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import androidx.navigation.NavHostController
 import it.polito.database.AppViewModel
 import it.polito.database.R
@@ -48,9 +54,12 @@ fun PaeseELinguaScreen(viewModel: AppViewModel, navController: NavHostController
     var selectedCountry = remember { mutableStateOf<Paese?>(null)}
     var selectedLanguage = remember { mutableStateOf<Lingua?>(null)}
 
-    Column (Modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally){
+    Column (
+        Modifier
+            .fillMaxSize()
+            .padding(top = 180.dp, start = 16.dp, end = 16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    {
     Column {
         Row(modifier = Modifier.fillMaxWidth()) {
             Text(
@@ -58,7 +67,7 @@ fun PaeseELinguaScreen(viewModel: AppViewModel, navController: NavHostController
                 color = Color.Black,
                 fontFamily = fontFamily,
                 fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
+                fontSize = 20.sp,
                 textAlign = TextAlign.Start
             )
         }
@@ -68,6 +77,7 @@ fun PaeseELinguaScreen(viewModel: AppViewModel, navController: NavHostController
                 country -> selectedCountry.value = country
         }
     }
+    Spacer(modifier = Modifier.height(16.dp) )
     Column {
         Row(modifier = Modifier.fillMaxWidth()) {
             Text(
@@ -75,7 +85,7 @@ fun PaeseELinguaScreen(viewModel: AppViewModel, navController: NavHostController
                 color = Color.Black,
                 fontFamily = fontFamily,
                 fontWeight = FontWeight.Bold,
-                fontSize = 18.sp,
+                fontSize = 20.sp,
                 textAlign = TextAlign.Start
             )
         }
@@ -105,6 +115,7 @@ fun Paese(
     if (selectedCountry===null){
         chosenCountry="Austria";
     }
+    var textFiledSize by remember { mutableStateOf(Size.Zero) }
 
     val icon = if (isExpanded){
         painterResource(id = R.drawable.frecciasopra)
@@ -112,10 +123,10 @@ fun Paese(
 
     Column(modifier = Modifier.fillMaxWidth()){
         OutlinedTextField(
-            shape = MaterialTheme.shapes.large,
+            shape = RoundedCornerShape(15.dp),
             value = chosenCountry,
             onValueChange = {},
-            readOnly = false,
+            readOnly = true,
             trailingIcon = {
                 Icon(icon , "", tint = Color.Black,
                     modifier = Modifier
@@ -123,28 +134,28 @@ fun Paese(
                         .size(16.dp))
             },
             colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                focusedBorderColor = Color.Black,
-                unfocusedBorderColor = if (isExpanded) MaterialTheme.colorScheme.onSecondary else Color.Black,
+                containerColor = Color.White,
                 textColor = MaterialTheme.colorScheme.onBackground
             ),
             textStyle = TextStyle(
                 fontFamily = fontFamily,
                 fontWeight = FontWeight.Normal,
-                fontSize = 18.sp,
-                color = Color.Black),
+                fontSize = 18.sp),
             modifier = Modifier
                 .fillMaxWidth()
-
+                .border(2.dp, Color.Black, RoundedCornerShape(15.dp))
+                .onGloballyPositioned { coordinates ->
+                    textFiledSize = coordinates.size.toSize()
+                }
         )
         DropdownMenu(
-            offset = DpOffset(0.dp, (4).dp),
+            offset = DpOffset(0.dp, (10).dp),
             modifier = Modifier
-                .padding(vertical = 2.dp)
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+                .background(MaterialTheme.colorScheme.onPrimary)
                 .verticalScroll(rememberScrollState())
-                .height(400.dp)
-                .border(1.dp, MaterialTheme.colorScheme.onPrimary),
+                .height(280.dp)
+                .border(2.dp, MaterialTheme.colorScheme.onBackground),
             expanded = isExpanded,
             onDismissRequest = { isExpanded = false },
         ) {
@@ -165,7 +176,7 @@ fun Paese(
                     }
                 )
                 if (index < countries.size - 1){
-                    Divider(thickness = 2.dp, color = Color(0x1A000000))
+                    Divider(thickness = 1.dp, color = Color(0x1A000000))
                 }
 
             }
@@ -188,17 +199,19 @@ fun Lingua(
     if (selectedLanguage===null){
         chosenLanguage="Italiano";
     }
+    var textFiledSize by remember { mutableStateOf(Size.Zero) }
 
     val icon = if (isExpanded){
         painterResource(id = R.drawable.frecciasopra)
     } else painterResource(id = R.drawable.frecciasotto)
 
+
     Column(modifier = Modifier.fillMaxWidth()){
         OutlinedTextField(
-            shape = MaterialTheme.shapes.large,
+            shape = RoundedCornerShape(15.dp),
             value = chosenLanguage,
             onValueChange = {},
-            readOnly = false,
+            readOnly = true,
             trailingIcon = {
                 Icon(icon , "", tint = Color.Black,
                     modifier = Modifier
@@ -206,28 +219,28 @@ fun Lingua(
                         .size(16.dp))
             },
             colors = TextFieldDefaults.outlinedTextFieldColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                focusedBorderColor = Color.Black,
-                unfocusedBorderColor = if (isExpanded) MaterialTheme.colorScheme.onSecondary else Color.Black,
+                containerColor = Color.White,
                 textColor = MaterialTheme.colorScheme.onBackground
             ),
             textStyle = TextStyle(
                 fontFamily = fontFamily,
                 fontWeight = FontWeight.Normal,
-                fontSize = 18.sp,
-                color = Color.Black),
+                fontSize = 18.sp),
             modifier = Modifier
                 .fillMaxWidth()
-
+                .border(2.dp, Color.Black, RoundedCornerShape(15.dp))
+                .onGloballyPositioned { coordinates ->
+                    textFiledSize = coordinates.size.toSize()
+                }
         )
         DropdownMenu(
-            offset = DpOffset(0.dp, (4).dp),
+            offset = DpOffset(0.dp, (10).dp),
             modifier = Modifier
-                .padding(vertical = 2.dp)
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .width(with(LocalDensity.current) { textFiledSize.width.toDp() })
+                .background(MaterialTheme.colorScheme.onPrimary)
                 .verticalScroll(rememberScrollState())
-                .height(400.dp)
-                .border(1.dp, MaterialTheme.colorScheme.onPrimary),
+                .height(280.dp)
+                .border(2.dp, MaterialTheme.colorScheme.onBackground),
             expanded = isExpanded,
             onDismissRequest = { isExpanded = false },
         ) {
@@ -248,7 +261,7 @@ fun Lingua(
                     }
                 )
                 if (index < languages.size - 1){
-                    Divider(thickness = 2.dp, color = Color(0x1A000000))
+                    Divider(thickness = 1.dp, color = Color(0x1A000000))
                 }
 
             }
