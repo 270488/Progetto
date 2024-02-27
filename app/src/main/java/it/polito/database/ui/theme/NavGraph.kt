8d360642ac0b1d91557d102c8376a320f48c217a
andredelu98 <andrea.deluca1108@gmail.com>
2024-02-27 16:04:42 +0100

    Scus

--- app/src/main/java/it/polito/database/ui/theme/NavGraph.kt
+++ app/src/main/java/it/polito/database/ui/theme/NavGraph.kt
@@ -46,7 +46,7 @@ import it.polito.database.screens.SettingsScreen
 fun NavGraph(navController: NavHostController, viewModel: AppViewModel){
     NavHost(
         navController = navController,
-        startDestination = Screen.CorriereHome.route
+        startDestination = Screen.AuthenticationScreen.route
     ) {
         composable(route = Screen.Home.route) {
             HomePage(viewModel, navController)
